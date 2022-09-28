5e9413ee70f46d5c41b22eb2ff3cb2932b92fb69
Francis Onyido <frankloginss@gmail.com>
2022-09-28 16:42:39 -0400

    Conditional-components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const time = new Date();
+  const day = time.toLocaleString("en-us", {weekday: "long"})
+  const morning = time.getHours() >= 13 && time.getHours() <= 19;
+  let dayMessage;
+
+  if (day.toLowerCase() === "monday") {
+    dayMessage = `Happy ${day}`
+  } else if (day.toLowerCase() === "tuesday") {
+    dayMessage = `${day}, four days to go`;
+  } else if (day.toLowerCase() === "wednesday") {
+    dayMessage = `${day}, half way there`;
+  } else if (day.toLowerCase() === "thursday") {
+    dayMessage = `${day}, start planning the weekend`;
+  }else if (day.toLowerCase() === "friday") {
+    dayMessage = `Woo - hoo, the weekedn is coming!`;
+  } else {
+    dayMessage = "Stay calm and keep having fun";
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Conditional-components</h2>
+      <h1>{dayMessage}</h1>
+      {morning ? <h2>Have you had launch yet?</h2> : ""}
     </div>
   );
 }
